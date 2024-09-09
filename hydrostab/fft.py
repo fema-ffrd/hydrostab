7974c35be592005c43cc9bc4a61ea3110a2224a4
Thomas Williams <thomas.williams@wsp.com>
2024-09-09 10:15:37 -0400

    normalization

--- hydrostab/fft.py
+++ hydrostab/fft.py
@@ -8,8 +8,10 @@ from typing import Tuple
 def fft_stable(
     hydrograph: np.ndarray,
     sampling_rate: float = 1.0,
-    threshold_period: float = 10.0,
-    high_freq_threshold: float = 0.2,
+    unstable_period: float = 10.0,
+    threshold: float = 0.2,
+    normalize: bool = True,
+    relative: bool = False,
 ) -> Tuple[bool, float, np.ndarray, np.ndarray]:
     """Check if a time series hydrograph is stable using Fourier Transforms.
 
@@ -20,14 +22,18 @@ def fft_stable(
     sampling_rate: float, optional
         Sampling rate of the hydrograph data. i.e., the time between each data
         point.
-    threshold_period : float, optional
-        Time period for unstable frequencies. Oscillations happening faster
+    unstable_period : float, optional
+        Time period for unstable oscillations. Oscillations happening faster
         than this are considered problematic. Must be in the same units as
         the sampling rate.
-    high_freq_threshold : float, optional
-        Threshold for identifying high frequencies. If the proportion
-        of power in high-frequency components exceeds this threshold, the
-        hydrograph is considered unstable.
+    threshold : float, optional
+        Power threshold of unstable oscillations for classifying. If the
+        stability. If the proportion of power in high-frequency components
+        exceeds this threshold, the hydrograph is considered unstable.
+    normalize : bool, optional
+        Normalize hydrograph data to the range [0, 1].
+    relative : bool, optional
+        Adjust hydrograph data relative to the minimum value.
 
     Returns
     -------
@@ -41,6 +47,11 @@ def fft_stable(
         Frequencies of the power spectrum.
 
     """
+    if relative:
+        hydrograph = hydrograph - np.min(hydrograph)
+    if normalize:
+        hydrograph = hydrograph / np.max(hydrograph)
+
     # compute the Fourier Transform
     fft_values = np.fft.fft(hydrograph)
 
@@ -53,12 +64,12 @@ def fft_stable(
     power_spectrum = np.abs(fft_values)[: n // 2]
 
     # identify high-freq components
-    threshold_freq = 1.0 / threshold_period
+    threshold_freq = 1.0 / unstable_period
     high_freq_power = power_spectrum[freqs > threshold_freq]
 
     total_power = np.sum(power_spectrum)
     high_freq_proportion = np.sum(high_freq_power) / total_power
 
     # convert from numpy bool to Python bool
-    is_stable = bool(high_freq_proportion < high_freq_threshold)
+    is_stable = bool(high_freq_proportion < threshold)
     return is_stable, high_freq_proportion, power_spectrum, freqs
