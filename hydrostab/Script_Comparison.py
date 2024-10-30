b0d92cc5204f8e036a93497f6f96a09b5322dd0b
Qijue Chen <qijue.chen@wsp.com>
2024-10-30 09:27:59 -0700

    Added Fourier Transform to the comparison script

--- hydrostab/Script_Comparison.py
+++ hydrostab/Script_Comparison.py
@@ -273,6 +273,87 @@ def detect_abrupt_changes(data, percent_change, max_time_interval):
     
     return unstable_pt
 
+#%% Fourier Transform
+"""Using Fourier Transforms to analyze hydrograph stability."""
+
+from typing import Tuple
+
+
+def fft_stable(
+    hydrograph_path: np.ndarray,
+    sampling_rate: float = 1.0,
+    unstable_period: float = 10.0,
+    threshold: float = 0.2,
+    normalize: bool = True,
+    relative: bool = False,
+) -> Tuple[bool, float, np.ndarray, np.ndarray]:
+    """Check if a time series hydrograph is stable using Fourier Transforms.
+
+    Parameters
+    ----------
+    hydrograph : np.ndarray
+        Time series hydrograph data.
+    sampling_rate: float, optional
+        Sampling rate of the hydrograph data. i.e., the time between each data
+        point.
+    unstable_period : float, optional
+        Time period for unstable oscillations. Oscillations happening faster
+        than this are considered problematic. Must be in the same units as
+        the sampling rate.
+    threshold : float, optional
+        Power threshold of unstable oscillations for classifying. If the
+        stability. If the proportion of power in high-frequency components
+        exceeds this threshold, the hydrograph is considered unstable.
+    normalize : bool, optional
+        Normalize hydrograph data to the range [0, 1].
+    relative : bool, optional
+        Adjust hydrograph data relative to the minimum value.
+
+    Returns
+    -------
+    is_unstable: bool
+        True if the time series is unstable, False otherwise.
+    high_freq_proportion: float
+        Proportion of power in high-frequency components.
+    power_spectrum: np.ndarray
+        Power spectrum of the hydrograph data.
+    freqs: np.ndarray
+        Frequencies of the power spectrum.
+
+    """
+
+    df_hydrograph = pd.read_csv(hydrograph_path)
+    hydrograph = df_hydrograph["flow"].values
+    
+    if relative:
+        hydrograph = hydrograph - np.min(hydrograph)
+    if normalize:
+        hydrograph = hydrograph / np.max(hydrograph)
+
+    # compute the Fourier Transform
+    fft_values = np.fft.fft(hydrograph)
+
+    # compute frequencies, dropping negative frequencies
+    n = len(hydrograph)
+    d = 1.0 / sampling_rate
+    freqs = np.fft.fftfreq(n, d=d)[: n // 2]
+
+    # compute power spectrum, dropping negative frequencies
+    power_spectrum = np.abs(fft_values)[: n // 2]
+
+    # identify high-freq components
+    threshold_freq = 1.0 / unstable_period
+    high_freq_power = power_spectrum[freqs > threshold_freq]
+
+    total_power = np.sum(power_spectrum)
+    high_freq_proportion = np.sum(high_freq_power) / total_power
+
+    # convert from numpy bool to Python bool
+    is_stable = bool(high_freq_proportion < threshold)
+    return is_stable, high_freq_proportion, power_spectrum, freqs
+
+
+
 #%% Run Comparison
 
 path = 'C:/Users/USQC714579/Documents/hydrostab/tests/data/hydrographs/DSS_Hydrographs/'
@@ -285,6 +366,7 @@ for i in os.listdir(path):
 ls_firstDerivative = []
 ls_std = []
 ls_pt_change = []
+ls_fourier = []
 i = 0
 
 for f in files:
@@ -299,9 +381,19 @@ for f in files:
     # Point Change 
     unstable_pt = detect_abrupt_changes(f_path, 0.06, 10)
     
+    #Fourier Transform
+    is_stable, _, _, _ = fft_stable(f_path)
+    
     ls_firstDerivative.append(oscillation_label)
     ls_std.append(std_dev_label)
     ls_pt_change.append(unstable_pt)
+
+    if is_stable:
+        ls_fourier.append("Stable")
+    else:
+        ls_fourier.append("Unstable")
+    
+    
 
 
     
@@ -312,10 +404,14 @@ for i in range(len(ls_pt_change)):
         ls_pt_change_label.append( "Unstable")
     else:
         ls_pt_change_label.append("Stable")
-
+        
+        
 df_hydrograph = pd.DataFrame({'Hydrograph Title' : files,
                               'First Derivative' : ls_firstDerivative,
                               'Std' : ls_std,
-                              'Point Change' : ls_pt_change_label }, 
-                              columns=['Hydrograph Title','First Derivative', 'Std', 'Point Change'])
+                              'Point Change' : ls_pt_change_label,
+                             'Fourier Transform': ls_fourier }, 
+                              columns=['Hydrograph Title','First Derivative', 'Std', 'Point Change', 'Fourier Transform'])
+
+
 
